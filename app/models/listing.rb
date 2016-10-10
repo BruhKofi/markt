21dce91fdb8fe1279c121110507c3f2a94291e3f
Tynnee <victorasantekyei@yahoo.co.uk>
2016-10-10 19:04:32 +0200

    added validations for users and listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
 	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpeg"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	validates :name, :description, :price, presence: true
+	validates :price, numericality: { greater_than: 0 }
+	validates :image, attachment_presence: true
 end
